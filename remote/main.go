2f21b70918ebabc154af4830c0955a4da25ac01e
baqianxin <baqianxin@360.cn>
2020-07-23 17:53:35 +0800

    chromedp

--- remote/main.go
+++ remote/main.go
@@ -5,15 +5,50 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 
+	"github.com/chromedp/cdproto/network"
 	"github.com/chromedp/chromedp"
 )
 
-var flagDevToolWsUrl = flag.String("devtools-ws-url", "", "DevTools WebSsocket URL")
+var flagDevToolWsUrl = flag.String("devtools-ws-url", "ws://127.0.0.1:9222/devtools/page/641D8D9F3CAB30A96D3C9C8FC6DAC17B", "DevTools WebSocket URL")
 
 func main() {
+	// Create contexts.
+	opts := append(chromedp.DefaultExecAllocatorOptions[:], chromedp.Flag("headless", false))
+	actx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+	ctx, cancel := chromedp.NewContext(actx)
+
+	// Call cancel() to close Chrome on some condition.
+	if false {
+		cancel()
+	}
+
+	// Custom header.
+	headers := map[string]interface{}{
+		"X-Header": "my request header",
+	}
+	var body string
+	task := chromedp.Tasks{
+		network.Enable(),
+		network.SetExtraHTTPHeaders(network.Headers(headers)),
+		chromedp.Navigate("https://duckduckgo.com"),
+		chromedp.OuterHTML("html", &body),
+	}
+	defer cancel()
+	// Run task.
+	err := chromedp.Run(ctx, task)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Body of duckduckgo.com starts with:")
+	log.Println(body[0:10])
+}
+func main2() {
 	flag.Parse()
+	fmt.Println(*flagDevToolWsUrl)
 	if *flagDevToolWsUrl == "" {
 		log.Fatal("must specify -devtools-ws-url")
 	}
@@ -37,5 +72,5 @@ func main() {
 	}
 
 	log.Println("Body of duckduckgo.com starts with:")
-	log.Println(body[0:100])
+	log.Println(body[0:10])
 }
